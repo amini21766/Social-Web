65cd8f0806847546521c7c4a33bcf7a5c7d459e2
amini21766 <amini21766@gmail.com>
2024-08-19 22:29:58 +0430

    Convert files to LF line endings

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-700 text-center">Welcome to App!</h1>
+    </div>
+  );
+}
